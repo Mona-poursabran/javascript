c9e3838b4f20b851ae8246dab8d4085996509fc8
mona <poursabranenglish@gmail.com>
2021-11-08 21:03:34 +0330

    add tasks and save in the local storage

--- app.js
+++ app.js
@@ -18,6 +18,41 @@ $(document).ready(function(){
                     })};
     };
 
- 
+  // onclick function=> Add tasks
+    $('#task-form').on('submit', function(event){
+        event.preventDefault();
+        inputTask = $('#task').val().trim();
+    
+        if($.trim(inputTask)==''){
+            // return null;
+            alert('ابتدا تسک را وارد کرده و بعد دکمه اضافه را کلید کنید');
+        }else{
+           const tagLi = $("<li class='list-group-item d-flex justify-content-between'></li>");
+           const tagI = $("<i class='fas fa-times text-danger mr-auto delete-item'></i>");
+           
+           $('.list-group').append($(tagLi).append(inputTask).append(tagI));
+           alert('تسک ذخیره شد  :)')
+        };
+
+        iElement = $(".delete-item");
+        $(iElement).click(function(){
+           $(this).parent().remove();
+        })
+
+        let tasks ;
+        tasks = []
+        if(localStorage.getItem('tasks') === null){
+            tasks= [];
+        }else{
+            tasks =JSON.parse(localStorage.getItem('tasks'));
+            
+        }
+        tasks.push(inputTask);
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        
+    });
+
+
+
 
 });
